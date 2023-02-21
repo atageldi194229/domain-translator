e06e9a7e0d24509ac987e586372b4860673362d6
atageldi194229 <atageldi194229@gmail.com>
2023-02-22 00:02:10 +0500

    init

---
+++ index.js
@@ -0,0 +1,21 @@
+const dns = require('dns');
+const v1 = require("./v1.json");
+const fs = require("fs");
+
+let count = 0;
+let results = [];
+for (let { domain } of v1.result.records) {
+    dns.lookup(domain, (err, address, family) => {
+        if(err) throw err;
+        
+        results.push({ domain, address });
+
+        count++;
+    
+        if (count !== v1.result.count) return;
+
+        fs.writeFileSync("result.json", JSON.stringify(results));
+
+        console.log("Successfully done.");
+    });
+}
